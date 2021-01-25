e75a8433628e300e3d2dea4bcc387ee6182e1d82
Max Johnson <maxwell.johnson@realitymine.com>
2021-01-25 18:40:01 +0000

    made a start on welcome & login page

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 
-import Login from './src/components/Login';
-import User from './src/components/User';
+import Login from './src/components/login-component';
 
-class HellowWorld extends Component {
+// import Welcome from './src/components/welcome-component';
+
+// import User from './src/components/User';
+
+class App extends Component {
     render() {
         return (
-            <View>
-                <User name="Max" email="maxj131@hotmail.com"/>
-                {/* <User name="Maxwell" email="maxwell.johnson@realitymine.com"/> */}
-                {/* <User name="Macks" email="maxj131@gmail.com"/> */}
-                <Login />
-            </View>
+            // <View styles={styles.flexContainer}>
+                // {/* <User name="Max" email="maxj131@hotmail.com"/> */}
+                // {/* <User name="Maxwell" email="maxwell.johnson@realitymine.com"/> */}
+                // {/* <User name="Macks" email="maxj131@gmail.com"/> */}
+            // <Welcome />
+            <Login />
+            // </View>
         );
     }
 }
 
-export default HellowWorld;
+// const styles = StyleSheet.create({
+//     flexContainer: {
+//         flex: 1,
+//         backgroundColor: 'steelblue',
+//     },
+// });
+
+export default App;
---
+++ src/styles/global-styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+'use strict';
+export default StyleSheet.create({
+    primaryColor: {
+        color: '#F06543',
+    },
+    secondaryColor: {
+        color: '#F09D51',
+    },
+    textLight: {
+        color: '#F09D51',
+    },
+    textDark: {
+        color: '#F09D51',
+    },
+    backgroundColor: {
+        backgroundColor: '#E0DFD5',
+    },
+});
